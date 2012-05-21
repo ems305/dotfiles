8a3716585142eff59651f22b46fe42633737a977
Taylor Smith <mail@taylorsmith.net>
2012-05-21 12:09:40 -0600

    Remove dotfiles bin directory from /usr/local/bin:/usr/local/Cellar/ruby/1.9.2-p290/bin:/usr/local/sbin:~/.dotfiles/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/X11/bin since it longer exists

--- shell/env.sh
+++ shell/env.sh
@@ -4,7 +4,6 @@
 export EDITOR="vi"
 export CLICOLOR=1
 
-export PATH="~/.dotfiles/bin:$PATH"
 export PATH="/usr/local/sbin:$PATH"
 export PATH="/usr/local/Cellar/ruby/1.9.2-p290/bin:$PATH"
 export PATH="/usr/local/bin:$PATH"
