e8ed3dcbff5a581c2b084586d0c41463322d3900
Taylor Smith <mail@taylorsmith.net>
2012-07-29 14:42:36 -0600

    Add hushlogin and clean up environment variables

--- shell/env.sh
+++ shell/env.sh
@@ -3,18 +3,9 @@
 
 export EDITOR="vi"
 export CLICOLOR=1
-
 export PATH="/usr/local/sbin:$PATH"
 export PATH="/usr/local/Cellar/ruby/1.9.2-p290/bin:$PATH"
 export PATH="/usr/local/bin:$PATH"
-export PATH="/Users/taylor/.dotfiles/bin:$PATH"
-
+export PATH="$HOME/.dotfiles/bin:$PATH"
 export NODE_PATH="/usr/local/lib/node_modules"
-
-export ENVIRONMENT="development"
-
-export LOLCOMMITS_DEVICE="Display iSight"
-
 source ~/.secrets/shell/env.sh
-
-[[ -s $HOME/.tmuxinator/scripts/tmuxinator ]] && source $HOME/.tmuxinator/scripts/tmuxinator
