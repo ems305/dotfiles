1626fe69ac3b195b6fd003056354c948c16d8708
Taylor Smith <mail@taylorsmith.net>
2012-10-05 12:29:21 -0600

    Remove things I'm not using

--- shell/env.sh
+++ shell/env.sh
@@ -5,6 +5,8 @@ export EDITOR="vi"
 export CLICOLOR=1
 export PATH="/usr/local/sbin:$PATH"
 export PATH="/usr/local/bin:$PATH"
+export PATH="/usr/local/share/npm/bin:$PATH"
 export PATH="$HOME/.dotfiles/bin:$PATH"
+
 export NODE_PATH="/usr/local/lib/node_modules"
 source ~/.secrets/shell/env.sh
