ffd8824ae69577b2a561410e620c9a49d32a1890
Taylor Smith <mail@taylorsmith.net>
2012-11-13 10:52:32 -0700

    Add haml/markdown, and some shell updates

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,7 +1,7 @@
 # General Purpose
 # =============================================================================
 
-alias ls='ls -lh'                                             # Default listings to the long version with unit suffixes
+# alias ls='ls -lh'                                             # Default listings to the long version with unit suffixes
 alias la='ls -la'                                             # Include directory entries whose names begin with a dot
 alias ..='cd ..'                                              # Go up one directory
 alias cdd='cd -'                                              # Go to previous directory
@@ -50,12 +50,12 @@ alias hc='h run console'                                      # Initiate heroku 
 # =============================================================================
 
 alias g='git'                                                 # 1 letter is better than 3
-alias gd='git diff'                                           # Diff shortcut
-alias gc='git commit'                                         # Commit shortcut
-alias gca='git commit -a'                                     # Stage all tracked and modified files
-alias gco='git checkout'                                      # Checkout shortcut
-alias gb='git branch'                                         # Branch shortcut
-alias gs='git status -sb'                                     # Simple git status view
+# alias gd='git diff'                                           # Diff shortcut
+# alias gc='git commit'                                         # Commit shortcut
+# alias gca='git commit -a'                                     # Stage all tracked and modified files
+# alias gco='git checkout'                                      # Checkout shortcut
+# alias gb='git branch'                                         # Branch shortcut
+# alias gs='git status -sb'                                     # Simple git status view
 alias gitfix='rm -f .git/index && git reset'                  # Fixes corrupted git index
 alias killsvn='find . -type d -name .svn | xargs rm -rf'      # Remove all .svn folders
 
